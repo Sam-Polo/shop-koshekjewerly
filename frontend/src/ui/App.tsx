4b3e8ebfec68fee56058281eeab13d507bc87d5a
Semyon Polovodov <s4ns4l4ssn4ss@mail.ru>
2025-11-03 00:18:37 +0500

    design: fix

--- frontend/src/ui/App.tsx
+++ frontend/src/ui/App.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react'
 import WebApp from '@twa-dev/sdk'
 import React from 'react'
 
-import berriesImage from '../../assets/berries-category.jpg'
-import neckImage from '../../assets/neck-category.jpg'
-import handsImage from '../../assets/hands-category.jpg'
-import earsImage from '../../assets/ears-category.jpg'
-import certificateImage from '../../assets/certificate-category.jpg'
-import logoImage from '../../assets/logo.png'
-import backgroundImage from '../../assets/background.jpg'
+import berriesImage from '../assets/berries-category.jpg'
+import neckImage from '../assets/neck-category.jpg'
+import handsImage from '../assets/hands-category.jpg'
+import earsImage from '../assets/ears-category.jpg'
+import certificateImage from '../assets/certificate-category.jpg'
+import logoImage from '../assets/logo.png'
+import backgroundImage from '../assets/background.jpg'
 
 type Category = {
   key: string
